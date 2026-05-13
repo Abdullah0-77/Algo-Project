35c2e5add631d190fb62c7ddffdc56fd32049bbd
Abdullah0-77 <3bdallamohamed2007@gmail.com>
2026-05-13 21:16:10 +0300

    code

---
+++ Task.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void swapValues(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void merge(vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    vector<int> L(n1), R(n2);
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int i = 0; i < n2; i++)
+        R[i] = arr[mid + 1 + i];
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<int>& arr, int left, int right) {
+    if (left >= right)
+        return;
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
+}
+
+void wiggleSortNONREC(vector<int>& nums) {
+    vector<int> sorted = nums;
+    mergeSort(sorted, 0, sorted.size() - 1);
+
+    int n = nums.size();
+
+    int left = (n - 1) / 2;
+    int right = n - 1;
+
+    for (int i = 0; i < n; i++) {
+
+        if (i % 2 == 0) {
+            nums[i] = sorted[left];
+            left--;
+        }
+        else {
+            nums[i] = sorted[right];
+            right--;
+        }
+    }
+}
+
+void recWiggleSort(vector<int>& nums,
+                   vector<int>& sorted,
+                   int& left,
+                   int& right,
+                   int i) {
+
+    if (i == nums.size())
+        return;
+    if (i % 2 == 0) {
+        nums[i] = sorted[left];
+        left--;
+    }
+    else {
+        nums[i] = sorted[right];
+        right--;
+    }
+    recWiggleSort(nums, sorted, left, right, i + 1);
+}
+
+void wiggleSortREC(vector<int>& nums) {
+    vector<int> sorted = nums;
+    mergeSort(sorted, 0, sorted.size() - 1);
+    int n = nums.size();
+    int left = (n - 1) / 2;
+    int right = n - 1;
+    recWiggleSort(nums, sorted, left, right, 0);
+}
+
+
+
+
+int main() {
+    vector<int> nums;
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; ++i) {
+        int num;
+        cin >> num;
+        nums.push_back(num);
+    }
+    vector<int> nums2 = nums;
+    wiggleSortNONREC(nums);
+    cout << "Wiggle Sorted Array (Non-Recursive): ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+    wiggleSortREC(nums2);
+    cout << "Wiggle Sorted Array (Recursive): ";
+    for (int num : nums2) {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
